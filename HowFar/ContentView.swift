f3121f9642285e102283d68126fc0df3f22355b2
Joe Gallegos <joebgallegos@gmail.com>
2020-06-19 13:58:07 -0500

    Initial Commit

---
+++ HowFar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HowFar
+//
+//  Created by Joe Gallegos on 6/19/20.
+//  Copyright © 2020 Joe Gallegos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
